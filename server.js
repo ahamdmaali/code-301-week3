478c436ffa17f0c1ef793c66f1d38593cbcee7c5
ahamdmaali <ahmad.maali95@outlook.com>
2021-05-05 14:12:05 +0300

    fixing errors

--- server.js
+++ server.js
@@ -3,47 +3,49 @@
 require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3080;
 const server = express();
-
-
+const cors = require('cors');
+server.use(cors());
 server.use(express.urlencoded({extended:true}));
 server.set('view engine','ejs');
+server.use(express.static("./public"));
 
 server.get('/',(req,res)=>{
     res.render('./pages/index')
 })
 server.post('/searches',searchHandler);
 
-function searchHandler(req,res){
-     let arrayOfBooks=[];
-     let bookName= req.body.title
-     let bookAuther= req.body.auther
-    let url= `https://www.googleapis.com/books/v1/volumes?q=in${bookName}+${bookAuther}`
-   
-    superagent.get(url)
-    .then(booksData=>{
 
-      booksData.body.items.forEach(item => {
-        arrayOfBooks.push(new Book(item));
-        res.render('pages/searches/new',{bookDeatails: arrayOfBooks});
-      })  
+function searchHandler(req,res){
+  
+  let search = req.body.search;
+  let term = req.body.type;
+  let url = `https://www.googleapis.com/books/v1/volumes?q=${term}:${search}`;
+  superagent.get(url)
+  .then(booksdata => {
+
+    let bookdata = booksdata.body.items;
+    let bookInfo = bookdata.map((item) => {
+      return new Book(item);
+    });
+      res.render('pages/searches/new',{bookDeatails:bookInfo});
     })
-    
-    .catch(error=>{
+     .catch(error=>{
         res.send(error);
     })
- 
+    
 }
 
- let Book = function(bookObj){
-    this.title= bookObj.volumeInfo.title;
-    this.auther= bookObj.volumeInfo.authors[0];
-    this.publisher= bookObj.volumeInfo.publisher;
-    this.publishedDate=bookObj.volumeInfo.publishedDate;
-    this.imageLinks= bookObj.volumeInfo.imageLinks.smallThumbnail;
-    this.canonicalVolumeLink= bookObj.volumeInfo.canonicalVolumeLink;
-    
+function Book(bookObj) {
+  this.title= bookObj.volumeInfo.title;
+  this.auther= bookObj.volumeInfo.authors;
+  this.description = bookObj.volumeInfo.description;
+  this.publisher= bookObj.volumeInfo.publisher;
+  this.publishedDate=bookObj.volumeInfo.publishedDate;
+  this.imageLinks= bookObj.volumeInfo.imageLinks.smallThumbnail;
+  this.canonicalVolumeLink= bookObj.volumeInfo.canonicalVolumeLink;
+  
 };
 server.listen(PORT,()=>{
     console.log(`listening to port ${PORT}`)
