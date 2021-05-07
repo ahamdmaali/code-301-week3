f6830716b5289c9af19b52ecda02f98465ce353c
ahamdmaali <ahmad.maali95@outlook.com>
2021-05-07 17:50:56 +0300

    add update and delete methods

--- server.js
+++ server.js
@@ -15,87 +15,79 @@ server.get('/',(req,res)=>{
   res.render('pages/index')
 })
 server.post('/searches',searchHandler);
-server.get('/books/:id',bookDatailsHandler)
+server.post('/books', bookSelectHandelr);
+server.get('/books/:id', bookDetailsHandelr);
 server.put('/updateBook/:id',updateBookHandler);
 server.delete('/deleteBook/:id',deleteBookHandler);
 
+function Book(bookObj) {
+  this.title= bookObj.volumeInfo.title;
+  this.auther= bookObj.volumeInfo.authors;
+  this.description = bookObj.volumeInfo.description;
+  this.publisher= bookObj.volumeInfo.publisher;
+  this.publishedDate=bookObj.volumeInfo.publishedDate;
+  this.imageLinks= bookObj.volumeInfo.imageLinks.smallThumbnail;
+  this.canonicalVolumeLink= bookObj.volumeInfo.canonicalVolumeLink;
+  
+};
 
-
-function bookDatailsHandler(req,res){
-  let bookId = req.params.id;
-  let SQL = `SELECT * FROM books WHERE id=$1;`;
-  let safeValue = [bookId];
-  client.query(SQL,safeValue)
-  .then(data=>{
-    res.render('pages/books/show',{bookDeatails:data.rows[0]});
-  })
-}
-let booksArr=[];
 function searchHandler(req,res){
   
-let SQL = "SELECT * FROM books;";
-client.query(SQL)
-.then(results=>{
- console.log(results.rows)
- res.render('pages/searches/new',{bookDeatails: results.rows[0]});
-})
-     let bookName= req.body.title
-     let bookAuther= req.body.auther
-    let url= `https://www.googleapis.com/books/v1/volumes?q=${bookName}+${bookAuther}`
-   
-    superagent.get(url)
-    .then(booksData=>{
-      let SQL = `INSERT INTO books (title,auther,publisher,publishedDate,imageLinks,canonicalVolumeLink) VALUES
-  ($1,$2,$3,$4,$5,$6);`;
-      booksData.body.items.forEach(item => {
-      let safeValues= [new Book(item).title,new Book(item).auther,new Book(item).publisher,new Book(item).publishedDate,new Book(item).imageLinks,new Book(item).canonicalVolumeLink];
-      
-      client.query(SQL,safeValues)
-      .then(results=>{
-       console.log(results.rows)
-       
-      })
-      res.render('pages/searches/new',{bookDeatails: results.rows[0]});
-      })
-     
-    
-    })
-    .catch(error=>{
-        res.send(error);
-    })
-}
-function updateBookHandler(req,res){
+  let search = req.body.search;
+  let term = req.body.type;
+  let url = `https://www.googleapis.com/books/v1/volumes?q=${term}:${search}`;
+  superagent.get(url)
+  .then(booksdata => {
 
-  let {title,auther,publisher,publishedDate,imageLinks,canonicalVolumeLink} = req.body;
-  let SQL = `UPDATE tasks SET title=$1,auther=$2,publisher=$3,publishedDate=$4,imageLinks=$5,canonicalVolumeLink=$6 WHERE id=$7;`;
-  let safeValues = [title,auther,publisher,publishedDate,imageLinks,canonicalVolumeLink,req.params.id];
-  client.query(SQL,safeValues)
-  .then(()=>{
+    let bookdata = booksdata.body.items;
+    let bookInfo = bookdata.map((item) => {
+      return new Book(item);
+    });
+      res.render('pages/searches/new',{bookDeatails:bookInfo});
+    })
+     .catch(error=>{
+        res.send(error);
+    });
+};
+
+function bookSelectHandelr(req, res) {
+  let SQL = `INSERT INTO books (title,author,imageLinks,descriptions) VALUES ($1,$2,$3,$4) RETURNING *;`;
+  let safeValues = [req.body.title, req.body.authors,req.body.imageLinks,req.body.description];
+  
+  client.query(SQL, safeValues)
+  .then(results => {
+    res.redirect(`books/${results.rows[0].id}`);
+  }) 
+  .catch(error => {
+    res.send(error);
+  });
+};
+
+function bookDetailsHandelr(req, res) {
+  let SQL = 'SELECT * FROM books WHERE id=$1;';
+  let safeValue = [req.params.id];
+  client.query(SQL,safeValue).then(results => {
+    res.render('pages/books/show',{database:results.rows[0]});
+  }) .catch(error => {
+    res.send(error);
+  });
+};
+function updateBookHandler(req,res) {
+  let SQL = `UPDATE books SET imageLinks=$1,title=$2,author=$3,description=$4 WHERE id=$5;`;
+  let safeValues = [req.body.imageLinks, req.body.title, req.body.author, req.body.description,req.params.id];
+  client.query(SQL, safeValues)
+  .then(() => {
     res.redirect(`/books/${req.params.id}`);
   })
 }
-function deleteBookHandler(req,res){
-  
-  let SQL=`DELETE FROM books WHERE id=$1;`;
-  let safevalue=[req.params.id];
-  client.query(SQL,safevalue)
-  .then(()=>{
-    res.redirect('/');
-  })
+function deleteBookHandler(req, res) {
+  let SQL = `DELETE FROM books WHERE id=$1;`;
+  let safeValue = [req.params.id];
+  client.query(SQL, safeValue).then(() => {
+    res.redirect(`/`)
+  });
 }
-
-
-
-
- let Book = function(bookObj){
-    this.title= bookObj.volumeInfo.title;
-    this.auther= bookObj.volumeInfo.authors[0];
-    this.publisher= bookObj.volumeInfo.publisher;
-    this.publishedDate=bookObj.volumeInfo.publishedDate;
-    this.imageLinks= bookObj.volumeInfo.imageLinks.smallThumbnail;
-    this.canonicalVolumeLink= bookObj.volumeInfo.canonicalVolumeLink;
-    booksArr.push(this)
-};
-server.listen(PORT,()=>{
-    console.log(`listening to port ${PORT}`)
-})
+client.connect()
+  .then(() => {
+    server.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+  })
